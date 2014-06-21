cbdf776c3ed8967a16fd6ec83579aab40fd01d08
Sander Heilbron <sanderheilbron@gmail.com>
2014-06-22 00:04:07 +0200

    v0.4

--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 # Google Analytics Vimeo Video Tracking
-Tracking Vimeo Player Events with Google Analytics.
+A Google Analytics plugin for measuring Vimeo Player Events. The plugin supports Universal Analytics, Classic Google Analytics, and Google Tag Manager.
 
 ## Usage
 Include the scripts in the body section of the HTML document, just before the `</body>` tag. You’ll need to be running on a web server instead of opening the file directly in your browser. Flash and JS security restrictions will prevent the API from working when run locally.
@@ -22,62 +22,111 @@ The iframe embeds a Vimeo video player and allows Vimeo to serve an HTML5 player
 
 ## Requirements
 * jQuery 1.4.3 or higher
-* Google Analytics Tracking Code (asynchronous)
+* Classic Google Analytics Tracking Code (asynchronous), Universal Analytics Tracking Code or Google Tag Manager container code
 * The end user must be using a browser that supports the HTML5 postMessage feature. Most modern browsers support postMessage, though Internet Explorer 7 does not support it.
 
+## Google Tag Manager
+If you'd like to integrate with Google Tag Manager, the dataLayer variable names are below.
+
+* Event Category = `{{eventCategory}}`
+* Event Action = `{{eventAction}}`
+* Event Label = `{{eventLabel}}`
+* Event Value = `{{eventValue}}`
+* Event Non-Interaction = `{{eventNonInteraction}}`
+
+Use the GTM event `Vimeo` to fire the Universal or Classic Google Analytics event tag.
+
+The GTM container tag could also be used to include the plugin inside a page containing the video. Create a custom HTML tag and trigger this tag so that this script fires on gtm.dom (`{{event}}` equals `gtm.dom`), meaning it will fire once the DOM is ready.
+
 ## Browser Support
-Tested in Chrome (21), Firefox (15), Safari (5,6), IE (8,9). Also tested on iOS.
+Tested in the latest versions of Chrome, Firefox, Safari, and IE. Also tested on iOS.
 
 ## Event Tracking
 All player events are only tracked once. Restarting the video will not reset the event trackers.
 
-### Default event trackers
+### Basic event trackers
 * Category: Vimeo
 * Action:
-	* **Started video**: when the video starts playing.
-	* **Paused video**: when the video is paused.
-	* **Completed video**: when the video reaches 100% completion.
+  * **Started video**: when the video starts playing.
+  * **Paused video**: when the video is paused.
+  * **Resumed video**: when the video starts playing when it was paused.
+  * **Completed video**: when the video reaches 100% completion.
+	* **Skipped video**: when the video is skipped forward or backward.
 * Label: URL of embedded video on Vimeo.
 
-#####Example
+##### Example Classic Analytics
 ```js
 _gaq.push(['_trackEvent', 'Vimeo', 'Started video', 'http://player.vimeo.com/video/22439234', undefined, true]);
 _gaq.push(['_trackEvent', 'Vimeo', 'Paused video', 'http://player.vimeo.com/video/22439234', undefined, true]);
+_gaq.push(['_trackEvent', 'Vimeo', 'Resumed video', 'http://player.vimeo.com/video/22439234', undefined, true]);
 _gaq.push(['_trackEvent', 'Vimeo', 'Completed video', 'http://player.vimeo.com/video/22439234', undefined, true]);
+_gaq.push(['_trackEvent', 'Vimeo', 'Skipped video forward or backward', 'http://player.vimeo.com/video/22439234', undefined, true]);
 ```
+
+##### Example Universal Analytics
+```js
+ga('send', 'event', 'Vimeo', 'Started video', 'http://player.vimeo.com/video/22439234', undefined, {'nonInteraction': 1});
+ga('send', 'event', 'Vimeo', 'Paused video', 'http://player.vimeo.com/video/22439234', undefined, {'nonInteraction': 1});
+ga('send', 'event', 'Vimeo', 'Resumed video', 'http://player.vimeo.com/video/22439234', undefined, {'nonInteraction': 1});
+ga('send', 'event', 'Vimeo', 'Completed video', 'http://player.vimeo.com/video/22439234', undefined, {'nonInteraction': 1});
+ga('send', 'event', 'Vimeo', 'Skipped video forward or backward', 'http://player.vimeo.com/video/22439234', undefined, {'nonInteraction': 1});
+```
+
+##### Example Google Tag Manager
+```js
+dataLayer.push({'event': 'Vimeo', 'eventCategory': 'Vimeo', 'eventAction': 'Started video', 'eventLabel': 'http://player.vimeo.com/video/22439234', 'eventValue': undefined, 'eventNonInteraction': true});
+dataLayer.push({'event': 'Vimeo', 'eventCategory': 'Vimeo', 'eventAction': 'Paused video', 'eventLabel': 'http://player.vimeo.com/video/22439234', 'eventValue': undefined, 'eventNonInteraction': true});
+dataLayer.push({'event': 'Vimeo', 'eventCategory': 'Vimeo', 'eventAction': 'Resumed video', 'eventLabel': 'http://player.vimeo.com/video/22439234', 'eventValue': undefined, 'eventNonInteraction': true});
+dataLayer.push({'event': 'Vimeo', 'eventCategory': 'Vimeo', 'eventAction': 'Completed video', 'eventLabel': 'http://player.vimeo.com/video/22439234', 'eventValue': undefined, 'eventNonInteraction': true});
+dataLayer.push({'event': 'Vimeo', 'eventCategory': 'Vimeo', 'eventAction': 'Skipped video forward or backward', 'eventLabel': 'http://player.vimeo.com/video/22439234', 'eventValue': undefined, 'eventNonInteraction': true});
+```
+
 ### Progress event trackers
 
 * Category: Vimeo
 * Action:
-	* **25% Progress**: when the video reaches 25% of the total video time.
-	* **50% Progress**: when the video reaches 50% of the total video time.
-	* **75% Progress**: when the video reaches 75% of the total video time.
+  * **25% Progress**: when the video reaches 25% of the total video time.
+  * **50% Progress**: when the video reaches 50% of the total video time.
+  * **75% Progress**: when the video reaches 75% of the total video time.
 * Label: URL of embedded video on Vimeo.
 
-#####Example
+##### Example Classic Analytics
 ```js
 _gaq.push(['_trackEvent', 'Vimeo', 'Played video: 25%', 'http://player.vimeo.com/video/22439234', undefined, true]);
 _gaq.push(['_trackEvent', 'Vimeo', 'Played video: 50%', 'http://player.vimeo.com/video/22439234', undefined, true]);
 _gaq.push(['_trackEvent', 'Vimeo', 'Played video: 75%', 'http://player.vimeo.com/video/22439234', undefined, true]);
 ```
-### Seek event tracker
-* Category: Vimeo
-* Action:
-	* **Skipped video**: when the video is skipped forward or backward.
-* Label: URL of embedded video on Vimeo.
 
-#####Example
+##### Example Universal Analytics
 ```js
-_gaq.push(['_trackEvent', 'Vimeo', 'Skipped video forward or backward', 'http://player.vimeo.com/video/22439234', undefined, true]);
+ga('send', 'event', 'Vimeo', 'Played video: 25%', 'http://player.vimeo.com/video/22439234', undefined, {'nonInteraction': 1});
+ga('send', 'event', 'Vimeo', 'Played video: 50%', 'http://player.vimeo.com/video/22439234', undefined, {'nonInteraction': 1});
+ga('send', 'event', 'Vimeo', 'Played video: 75%', 'http://player.vimeo.com/video/22439234', undefined, {'nonInteraction': 1});
+```
+
+##### Example Google Tag Manager
+```js
+dataLayer.push({'event': 'Vimeo', 'eventCategory': 'Vimeo', 'eventAction': 'Played video: 25%', 'eventLabel': 'http://player.vimeo.com/video/22439234', 'eventValue': undefined, 'eventNonInteraction': true});
+dataLayer.push({'event': 'Vimeo', 'eventCategory': 'Vimeo', 'eventAction': 'Played video: 50%', 'eventLabel': 'http://player.vimeo.com/video/22439234', 'eventValue': undefined, 'eventNonInteraction': true});
+dataLayer.push({'event': 'Vimeo', 'eventCategory': 'Vimeo', 'eventAction': 'Played video: 75%', 'eventLabel': 'http://player.vimeo.com/video/22439234', 'eventValue': undefined, 'eventNonInteraction': true});
 ```
 
 ### Bounce rate
-The event trackers do not impact bounce rate of the page which embeds the video. The value of the opt_noninteraction parameter is set to `true`.
+The event trackers do not impact bounce rate of the page which embeds the video. The value of the opt_noninteraction parameter is set to `true` or `1`.
 
 ## Issues
-Have a bug? Please create an issue here on GitHub!
+Have a bug? Please create an [issue](https://github.com/sanderheilbron/vimeo.ga.js/issues) here on GitHub!
+
+## Contributing
+Want to contribute? Great! Just fork the project, make your changes and open a [pull request](https://github.com/sanderheilbron/vimeo.ga.js/pulls).
 
 ## Changelog
+### 0.4 (June 22, 2014)
+* Support for Universal Analytics and Google Tag Manager.
+* 'Resumed video' added as default event tracker.
+* Code cleanup.
+* Updated documentation.
+
 ### 0.3 (May 15, 2014):
 * Checks if Vimeo iframe is present to prevent JS error on pages without iframe.
 * Fixed a bug when the page contains other iframes.
--- vimeo.ga.js
+++ vimeo.ga.js
@@ -1,139 +1,193 @@
 /*!
- * vimeo.ga.js | v0.3
+ * vimeo.ga.js | v0.4
  * Copyright (c) 2014 Sander Heilbron (http://www.sanderheilbron.nl)
  * MIT licensed
  */
 
-$(function() {
-    // Check for iframe
-    if (!$('#vimeo-player').length) {
+(function($) {
+  "use strict";
+
+  // Check for iframe
+  if (!$('#vimeo-player').length) {
+    // console.info('iframe not available');
+    return;
+  }
+
+  var f = $('#vimeo-player'),
+      url = f.attr('src').split('?')[0],      // Source URL
+      protocol = document.URL.split(':')[0],  // Domain protocol (http or https)
+      trackProgress = f.data('progress'),     // Data attribute to enable progress tracking
+      trackSeeking = f.data('seek'),          // Data attribute to enable seek tracking
+      gaTracker,
+      progress25 = false,
+      progress50 = false,
+      progress75 = false,
+      videoPlayed = false,
+      videoPaused = false,
+      videoResumed = false,
+      videoSeeking = false,
+      videoCompleted = false,
+      timePercentComplete = 0;
+
+  // Match protocol with what is in document.URL
+  if (url.match(/^http/) === null) {
+    url = protocol + ':' + url;
+  }
+
+  // Check which version of Google Analytics is used
+
+  // Universal Analytics (universal.js)
+  if (typeof ga === "function") {
+    gaTracker = 'ua';
+    // console.info('Universal Analytics');
+  }
+
+  // Classic Analytics (ga.js)
+  if (typeof _gaq !== "undefined" && typeof _gaq.push === "function") {
+    gaTracker = 'ga';
+    // console.info('Classic Analytics');
+  }
+
+  // Google Tag Manager (dataLayer)
+  if (typeof dataLayer !== "undefined" && typeof dataLayer.push === "function") {
+    gaTracker = 'gtm';
+    // console.info('Google Tag Manager');
+  }
+
+  // Listen for messages from the player
+  if (window.addEventListener) {
+    window.addEventListener('message', onMessageReceived, false);
+  } else {
+    window.attachEvent('onmessage', onMessageReceived, false);
+  }
+
+  // Send event to Classic Analytics, Universal Analytics or Google Tag Manager
+  function sendEvent(action) {
+    switch (gaTracker) {
+    case 'gtm':
+      dataLayer.push({
+        'event' : 'Vimeo',
+        'eventCategory' : 'Vimeo',
+        'eventAction' : action,
+        'eventLabel' : url,
+        'eventValue' : undefined,
+        'eventNonInteraction' : true
+      });
+      break;
+    case 'ua':
+      ga('send', 'event', 'Vimeo', action, url, undefined, {
+        "nonInteraction" : 1
+      });
+      break;
+    case 'ga':
+      _gaq.push(['_trackEvent', 'Vimeo', action, url, undefined, true]);
+      break;
+    }
+  }
+
+  // Handle messages received from the player
+  function onMessageReceived(e) {
+    if (e.origin.replace('https:', 'http:') !== "http://player.vimeo.com" || typeof gaTracker === 'undefined') {
+      // console.warn('Tracker is missing!');
+      return;
+    }
+    var data = JSON.parse(e.data);
+
+    switch (data.event) {
+    case 'ready':
+      onReady();
+      break;
+
+    case 'playProgress':
+      onPlayProgress(data.data);
+      break;
+
+    case 'seek':
+      if (trackSeeking && !videoSeeking) {
+        sendEvent('Skipped video forward or backward');
+        videoSeeking = true; // Avoid subsequent seek trackings
+      }
+      break;
+
+    case 'play':
+      if (!videoPlayed) {
+        sendEvent('Started video');
+        videoPlayed = true; // Avoid subsequent play trackings
+      } else if (!videoResumed && videoPaused) {
+        sendEvent('Resumed video');
+        videoResumed = true; // Avoid subsequent resume trackings
+      }
+      break;
+
+    case 'pause':
+      onPause();
+      break;
+
+    case 'finish':
+      if (!videoCompleted) {
+        sendEvent('Completed video');
+        videoCompleted = true; // Avoid subsequent finish trackings
+      }
+      break;
+    }
+  }
+
+  // Helper function for sending a message to the player
+  function post(action, value) {
+    var data = {
+      method: action
+    };
+
+    if (value) {
+      data.value = value;
+    }
+
+    f[0].contentWindow.postMessage(JSON.stringify(data), url);
+  }
+
+  function onReady() {
+    post('addEventListener', 'play');
+    post('addEventListener', 'seek');
+    post('addEventListener', 'pause');
+    post('addEventListener', 'finish');
+    post('addEventListener', 'playProgress');
+  }
+
+  function onPause() {
+    if (timePercentComplete < 99 && !videoPaused) {
+      sendEvent('Paused video');
+      videoPaused = true; // Avoid subsequent pause trackings
+    }
+  }
+
+  // Tracking video progress
+  function onPlayProgress(data) {
+    timePercentComplete = Math.round((data.percent) * 100); // Round to a whole number
+
+    if (!trackProgress) {
       return;
     }
 
-    var f = $('#vimeo-player'),
-        url = f.attr('src').split('?')[0], // source URL
-        protocol = document.URL.split(':')[0], // domain protocol (http or https)
-        trackProgress = f.data('progress'), // Data attribute to enable progress tracking
-        trackSeeking = f.data('seek'); // Data attribute to enable seek tracking
+    var progress;
 
-    // Match protocol with what is in document.URL
-    if (url.match(/^http/) === null) {
-        url = protocol + ':' + url;
+    if (timePercentComplete > 24 && !progress25) {
+      progress = 'Played video: 25%';
+      progress25 = true;
     }
 
-    // Listen for messages from the player
-    if (window.addEventListener) {
-        window.addEventListener('message', onMessageReceived, false);
-    } else {
-        window.attachEvent('onmessage', onMessageReceived, false);
+    if (timePercentComplete > 49 && !progress50) {
+      progress = 'Played video: 50%';
+      progress50 = true;
     }
 
-    // Handle messages received from the player
-    function onMessageReceived(e) {
-        if (e.origin.replace('https:', 'http:') !== "http://player.vimeo.com" || typeof _gaq === 'undefined') {
-         return;
-        }
-        var data = JSON.parse(e.data);
-
-        switch (data.event) {
-        case 'ready':
-            onReady();
-            break;
-
-        case 'playProgress':
-            onPlayProgress(data.data);
-            break;
-
-        case 'seek':
-            if (trackSeeking && !videoSeeking) {
-                _gaq.push(['_trackEvent', 'Vimeo', 'Skipped video forward or backward', url, undefined, true]);
-                videoSeeking = true; // Avoid subsequent seek trackings
-            }
-            break;
-
-        case 'play':
-            if (!videoPlayed) {
-                _gaq.push(['_trackEvent', 'Vimeo', 'Started video', url, undefined, true]);
-                videoPlayed = true; //  Avoid subsequent play trackings
-            }
-            break;
-
-        case 'pause':
-            onPause();
-            break;
-
-        case 'finish':
-            if (!videoCompleted) {
-                _gaq.push(['_trackEvent', 'Vimeo', 'Completed video', url, undefined, true]);
-                videoCompleted = true; // Avoid subsequent finish trackings
-            }
-            break;
-        }
+    if (timePercentComplete > 74 && !progress75) {
+      progress = 'Played video: 75%';
+      progress75 = true;
     }
 
-    // Helper function for sending a message to the player
-    function post(action, value) {
-        var data = {
-            method: action
-        };
-
-        if (value) {
-            data.value = value;
-        }
-
-        f[0].contentWindow.postMessage(JSON.stringify(data), url);
+    if (progress) {
+      sendEvent(progress);
     }
+  }
 
-    function onReady() {
-        post('addEventListener', 'play');
-        post('addEventListener', 'seek');
-        post('addEventListener', 'pause');
-        post('addEventListener', 'finish');
-        post('addEventListener', 'playProgress');
-        progress25 = false;
-        progress50 = false;
-        progress75 = false;
-        videoPlayed = false;
-        videoPaused = false;
-        videoSeeking = false;
-        videoCompleted = false;
-    }
-
-    function onPause() {
-     if (timePercentComplete < 99 && !videoPaused) {
-      _gaq.push(['_trackEvent', 'Vimeo', 'Paused video', url, undefined, true]);
-      videoPaused = true; // Avoid subsequent pause trackings
-      }
-     }
-
-    // Tracking video progress
-    function onPlayProgress(data) {
-        timePercentComplete = Math.round((data.percent) * 100); // Round to a whole number
-
-        if (!trackProgress) {
-         return;
-        }
-
-        var progress;
-
-        if (timePercentComplete > 24 && !progress25) {
-            progress = 'Played video: 25%';
-            progress25 = true;
-        }
-
-        if (timePercentComplete > 49 && !progress50) {
-            progress = 'Played video: 50%';
-            progress50 = true;
-        }
-
-        if (timePercentComplete > 74 && !progress75) {
-            progress = 'Played video: 75%';
-            progress75 = true;
-        }
-
-        if (progress) {
-            _gaq.push(['_trackEvent', 'Vimeo', progress, url, undefined, true]);
-        }
-    }
-
-});
+})(jQuery);
